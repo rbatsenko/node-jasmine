94caa1cceae6900dcc8c4a3ab62a919dcfc4d1e1
Roman Batsenko <rbatsenko@gmail.com>
2019-03-07 21:50:50 +0100

    add tests for all endpoints

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -10,11 +10,34 @@ describe('Server', () => {
   });
   describe('GET /', () => {
     let data = {};
-    beforeAll(() => {
+    beforeAll((done) => {
       Request.get('http://localhost:3000/', (error, response, body) => {
         data.status = response.statusCode;
         data.body = body;
-      })
-    })
+        done();
+      });
+    });
+    it('Status 200', () => {
+      expect(data.status).toBe(200);
+    });
+    it('Body', () => {
+      expect(data.body).toBe('Hi DUDE :)');
+    });
+  });
+  describe('GET /test', () => {
+    let data = {};
+    beforeAll((done) => {
+      Request.get('http://localhost:3000/test', (error, response, body) => {
+        data.status = response.statusCode;
+        data.body = JSON.parse(body);
+        done();
+      });
+    });
+    it('Status 500', () => {
+      expect(data.status).toBe(500);
+    });
+    it('Body', () => {
+      expect(data.body.message).toBe('This is an error response');
+    });
   });
 });
